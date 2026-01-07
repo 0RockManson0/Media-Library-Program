7ee4d64478cc5646003f464fa2cd146c30eb3c5b
Denis Chicharin <denischicharin@gmail.com>
2026-01-07 17:06:10 +0500

    Подогнал под pep 8

--- Rept_1.py
+++ Rept_1.py
@@ -1,9 +1,18 @@
+"""Отсортировать медиатеку.
+
+Полный список всех учеников, отсортированный по следующему ключу: класс (по
+возрастанию) + фамилия (по возрастанию).
+Использовать метод Хоара
+"""
+
+
 def quicksort(arr, low, high):
     """Реализация быстрой сортировки (Хоара)."""
     if low < high:
         pi = partition(arr, low, high)
         quicksort(arr, low, pi)
         quicksort(arr, pi + 1, high)
+
 
 def partition(arr, low, high):
     """Разбиение массива (схема Хоара)."""
@@ -13,17 +22,18 @@ def partition(arr, low, high):
 
     while True:
         i += 1
-        while compare_tracks(arr[i], pivot) < 0 and i < high: # Добавлена проверка i < high
+        while compare_tracks(arr[i], pivot) < 0 and i < high:
             i += 1
 
         j -= 1
-        while compare_tracks(arr[j], pivot) > 0 and j > low: # Добавлена проверка j > low
+        while compare_tracks(arr[j], pivot) > 0 and j > low:
             j -= 1
 
         if i >= j:
             return j
 
         arr[i], arr[j] = arr[j], arr[i]
+
 
 def compare_tracks(track1, track2):
     """Функция сравнения треков для сортировки."""
@@ -35,15 +45,17 @@ def compare_tracks(track1, track2):
         try:
             return int(track2[5]) - int(track1[5])
         except ValueError:
-            print(f"Ошибка: Некорректное значение прослушиваний: track1={track1[5]}, track2={track2[5]}")
-            return 0  # Возвращаем нейтральное значение при ошибке
+            print(f"Ошибка: Некорректное значение прослушиваний:"
+                  f" track1={track1[5]}, track2={track2[5]}")
+            return 0
+
 
 def load_tracks(filename="audiotracks.txt", encoding="utf-8"):
     """Чтение данных из файла."""
     tracks = []
     try:
         with open(filename, 'r', encoding=encoding) as f:
-            next(f) #Пропускаем строку заголовка
+            next(f)
             for line in f:
                 track_data = line.strip().split(',')
                 tracks.append(track_data)
@@ -52,13 +64,16 @@ def load_tracks(filename="audiotracks.txt", encoding="utf-8"):
         return []
     return tracks
 
+
 def print_tracks(tracks):
     """Вывод списка треков."""
     for track in tracks:
         print(','.join(track))
 
-# Основная часть
-tracks = load_tracks()
-if tracks:
-    quicksort(tracks, 0, len(tracks) - 1)
-    print_tracks(tracks)
+
+def main_sort_1():
+    """Основной модуль."""
+    tracks = load_tracks()
+    if tracks:
+        quicksort(tracks, 0, len(tracks) - 1)
+        print_tracks(tracks)
